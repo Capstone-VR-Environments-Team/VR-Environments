33ec5f6e00c2caea23a704d00e0b70a19cf423a5
vandykjr <vandykjr@rose-hulman.edu>
2026-01-14 10:37:18 -0500

    fixed errors from hide for time tests

--- Assets/Scripts/SphereManager.cs
+++ Assets/Scripts/SphereManager.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.Events;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -78,10 +77,10 @@ public class SphereManager : MonoBehaviour
     void SpawnNextSphere()
     {
         if (spheresCollected < this.spheres.Length && this.spheres[spheresCollected] != null) {
-            this.spheres[spheresCollected].SetActive(showTargets);
+            this.spheres[spheresCollected].GetComponent<MeshRenderer>().enabled = showTargets;
             currentSphere = this.spheres[spheresCollected];
             
-            if (showTargets && targetVisibleTime > 0)
+            if (!showTargets && targetVisibleTime > 0)
             {
                 StartCoroutine(HideSphereAfterDelay(targetVisibleTime));
             }
@@ -91,11 +90,14 @@ public class SphereManager : MonoBehaviour
     void ApplyVisibilitySettings()
     {
         if (leftHand != null)
+        {
             leftHand.GetComponent<MeshRenderer>().enabled = showHands;
+        }
         if (rightHand != null)
+        {
             rightHand.GetComponent<MeshRenderer>().enabled = showHands;
-
-        if (showHands && handVisibleTime > 0)
+        }
+        if (!showHands && handVisibleTime > 0)
         {
             StartCoroutine(HideHandsAfterDelay(handVisibleTime));
         }
@@ -107,7 +109,7 @@ public class SphereManager : MonoBehaviour
         
         if (currentSphere != null)
         {
-            currentSphere.SetActive(false);
+            currentSphere.GetComponent<MeshRenderer>().enabled = false;
             Debug.Log("Target hidden after visibility time expired");
         }
     }
@@ -115,11 +117,15 @@ public class SphereManager : MonoBehaviour
     IEnumerator HideHandsAfterDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
-        
+
         if (leftHand != null)
-            leftHand.SetActive(false);
+        {
+            leftHand.GetComponent<MeshRenderer>().enabled = false;
+        }
         if (rightHand != null)
-            rightHand.SetActive(false);
+        {
+            rightHand.GetComponent<MeshRenderer>().enabled = false;
+        }
 
         Debug.Log("Hands hidden after visibility time expired");
     }

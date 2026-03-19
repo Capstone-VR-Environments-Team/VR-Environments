7fb2d8f6b0bad363305566d08f841921a452df6e
rhit-kagayjm <kagayjm@rose-hulman.edu>
2026-03-19 16:59:51 -0400

    x button layer

--- Assets/Scripts/Interface/Pages/ReviewPastSessionsManager.cs
+++ Assets/Scripts/Interface/Pages/ReviewPastSessionsManager.cs
@@ -24,7 +24,6 @@ public class ReviewPastSessionsManager : MonoBehaviour
         {
             SetSessionInfo();
         }
-        UpdateScreen(AnalysisResultsStore.Instance.HasSessionInfo);
     }
 
     public void SetSessionInfo()

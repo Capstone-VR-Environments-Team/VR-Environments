76d7854355c903d2d3ed1a8ec0273ea9ca03fcbc
vandykjr <vandykjr@rose-hulman.edu>
2026-03-22 13:07:15 -0400

    trying to add custom background

---
+++ Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using System.IO;
+
+public class BackgroundManager : MonoBehaviour
+{
+    [Header("References")]
+    public Material skyboxMaterial;
+
+    private VideoPlayer videoPlayer;
+
+    public void UpdateBackgroundFromFile(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            Debug.LogError("Image file not found at: " + filePath);
+            return;
+        }
+
+        ResetSkybox();
+        byte[] fileData = File.ReadAllBytes(filePath);
+        Texture2D newTexture = new Texture2D(2, 2);
+
+        if (newTexture.LoadImage(fileData))
+        {
+            skyboxMaterial.SetTexture("_MainTex", newTexture);
+            RenderSettings.skybox = skyboxMaterial;
+        }
+    }
+
+    public void UpdateBackgroundFromVideo(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            Debug.LogError("Video file not found at: " + filePath);
+            return;
+        }
+
+        videoPlayer = GetComponent<VideoPlayer>();
+        if (videoPlayer == null) videoPlayer = gameObject.AddComponent<VideoPlayer>();
+
+        ResetSkybox();
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = filePath;
+        videoPlayer.renderMode = VideoRenderMode.APIOnly;
+        videoPlayer.isLooping = true;
+        videoPlayer.Play();
+
+        RenderSettings.skybox = skyboxMaterial;
+    }
+
+    private void ResetSkybox()
+    {
+        if (videoPlayer != null && videoPlayer.isPlaying) videoPlayer.Stop();
+        if (skyboxMaterial != null) skyboxMaterial.SetTexture("_MainTex", null);
+    }
+}

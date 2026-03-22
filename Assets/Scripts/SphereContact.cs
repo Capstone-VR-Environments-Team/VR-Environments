08705565b260dc33ef08b55f5f404bdaa3791f9e
vandykjr <vandykjr@rose-hulman.edu>
2026-03-22 17:20:03 -0400

    Attempt to fix hide error

--- Assets/Scripts/SphereContact.cs
+++ Assets/Scripts/SphereContact.cs
@@ -36,7 +36,7 @@ public class SphereContact : MonoBehaviour
         if (other.CompareTag("GameController") && hasBeenTriggered)
         {
             sphereManager.ApplyVisibilitySettings();
-            sphereManager.HideAfterExit();
+            //sphereManager.HideAfterExit();
         }
 
     }

3a523a39cd44615b84123cb013821aa4c5b35101
Doolittle <doolitge@rose-hulman.edu>
2026-03-23 08:32:18 -0400

    adding file selectors back

--- Assets/Scripts/SessionManager.cs
+++ Assets/Scripts/SessionManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class SessionManager : Singleton<SessionManager>
 {
-    public static string BaseDataPath;
+    private static string _path;
+    public static string BaseDataPath { get { _path ??= Application.persistentDataPath; return _path; } set => _path = value; }
     TrialSessionInformation _trialSessionInformation;
     TrialSettingsData _settings;
     private long _startTime;
@@ -63,8 +64,8 @@ public class SessionManager : Singleton<SessionManager>
         string folderName = $"{name}_{DateTime.Now:yyyyMMdd_HHmmss}";
 
         string rootPath = Path.Combine(Application.persistentDataPath, "TrialRuns");
-
-        _collectedDataDirectoryPath = Path.Combine(rootPath, folderName);
+        string folderPath = FileSelector.getFolderPath(rootPath);
+        _collectedDataDirectoryPath = Path.Combine(folderPath, folderName);
 
         if (!Directory.Exists(_collectedDataDirectoryPath)) {
             Directory.CreateDirectory(_collectedDataDirectoryPath);
@@ -73,7 +74,8 @@ public class SessionManager : Singleton<SessionManager>
     }
 
     public void SaveSettingsFile<T>(T data, string fileName) {
-        string filePath = Path.Combine(Application.persistentDataPath, "TrialFiles", fileName + ".json");
+        string directoryPath = Path.Combine(Application.persistentDataPath, "TrialFiles");
+        string filePath = FileSelector.getSaveFilePath(directoryPath, fileName, "json");
         FileManager.SaveJsonFile(data, filePath);
         Debug.Log($"JSON file saved to: {filePath}");
     }
